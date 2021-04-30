f8dd42d5b40dc25742e67d8ec5b9024d31fd2ab3
pedrosqra <pedro.lima@ccc.ufcg.edu.br>
2021-04-29 23:51:54 -0300

    serio plmdds

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://codextrainee.herokuapp.com/",
+  baseURL: "https://codextrainee.herokuapp.com/login",
 });
 
 export default api;
